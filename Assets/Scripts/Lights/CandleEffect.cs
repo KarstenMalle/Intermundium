34a9bc08ad081cfc7ea63e47957c39943ba4a2bf
Ian <iandahloliver@hotmail.com>
2022-12-23 12:56:13 +0100

    Added stuff to final level
    Added effect to torches, added sound to torches, fixed doors at both ends of stairs, added creepy sound to final room, and locked off main room.

--- Assets/Scripts/Lights/CandleEffect.cs
+++ Assets/Scripts/Lights/CandleEffect.cs
@@ -8,16 +8,25 @@ public class CandleEffect : MonoBehaviour
     [Tooltip("Offset effect so candles aren't in sync")]
     public float offset;
 
+    [Tooltip("How much the candle will sway")]
+    public double sway = 1d;
+
+    [Tooltip("How fast the candle will sway")]
+    public float speed = 1f;
+
+    private float init_intensity;
+
     // Start is called before the first frame update
     void Start()
     {
         candleLight = GetComponent<Light>();
+        init_intensity = candleLight.intensity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        double sin = Math.Abs(Math.Sin(Time.time + offset)) * 0.5 + 1;
+        double sin = Math.Sin(Time.time * speed + offset) * 0.5 * sway + init_intensity;
         candleLight.intensity = (float)sin;
     }
 }
